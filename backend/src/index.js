1e548565d2a96ed1650622744f45b76931adf8c4
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2026-06-29 16:25:35 +0700

    commit second

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://untearable-mouill-daniell.ngrok-free.dev',
   methods: ["GET", "POST"],
   credentials: true,
 }));
